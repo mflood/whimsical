419589e129287381d6c2eb83bfd1e563836dfa5e
Matthew Flood <matthew.flood@stitchfix.com>
2022-12-12 08:59:31 -0700

    Change output message

--- advent_of_code/run.sh
+++ advent_of_code/run.sh
@@ -4,4 +4,4 @@ source year_day.sh
 printf -v padded_day "%02d" ${ADVENT_DAY}
 
 python flood_advent/problem_${ADVENT_YEAR}_${padded_day}.py "${@:1}"
-echo "done running flood_advent/problem_${ADVENT_YEAR}_${padded_day}.py"
+echo python flood_advent/problem_${ADVENT_YEAR}_${padded_day}.py "${@:1}"
