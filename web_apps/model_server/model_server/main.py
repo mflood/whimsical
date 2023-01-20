8b95105e6959abac0fbe62a04ec0aefac86f9c31
mflood <hermannwest@gmail.com>
2023-01-19 21:24:24 -0700

    Add API Key auth

--- web_apps/model_server/model_server/main.py
+++ web_apps/model_server/model_server/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
 from model_server.models import UserIn, AddressIn, GeoOut
 
+from fastapi.security.api_key import APIKey
+from model_server.auth import get_api_key
+
+from fastapi import FastAPI, Body, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+
+
+api_keys = [
+    "prettyplease"
+]  # This is encrypted in the database
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+
+
+def api_key_auth(api_key: str = Depends(get_api_key)):
+    if api_key not in api_keys:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Forbidden"
+        )
+
 
 app = FastAPI()
 
@@ -8,8 +30,18 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/token")
+async def token():
+    return {"token": "prettyplease"}
+
 @app.get("/geocode", response_model=GeoOut)
 async def geocode(address_in: AddressIn):
+    resp = GeoOut(latitude=10.1, 
+    longitude=13)
+    return resp
+
+@app.get("/safe/geocode", response_model=GeoOut, dependencies=[Depends(api_key_auth)])
+async def geocode(address: str, country_code: str): 
     resp = GeoOut(latitude=10.1, 
     longitude=13)
     return resp
---
+++ web_apps/model_server/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ModelServer',
+      version='1.0',
+      description='Model Server',
+      author='Matthew Flood',
+      author_email='matthew.data.flood@gmail.com',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['model_server'],
+     )
